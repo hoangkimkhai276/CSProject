06278c36ebb02d851f9cae6f2a46ed425805bbe7
hahoangviet <hahoangviet126@gmail.com>
2021-09-12 20:35:41 +0200

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -71,6 +71,7 @@ public class Server {
                                 socket.close();
                                 return;
                             }
+                            break;
                         }
                         break;
                     }
